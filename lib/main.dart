7fbe157bfae2c7cff7bfb1fed9a33b7e060019e5
trungkien71297 <trungkien71297@gmail.com>
2023-09-08 11:14:19 +0700

    Add SPLASH SCREEN

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,13 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:nagn_2/blocs/home/home_bloc.dart';
 import 'package:nagn_2/di.dart';
 import 'package:nagn_2/ui/page/home_page.dart';
+import 'package:nagn_2/ui/page/splash_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   MobileAds.instance.initialize();
-  RequestConfiguration configuration = RequestConfiguration(testDeviceIds: ['30E815CD1E8D3F02F8402FDE4797BE29']);
+  RequestConfiguration configuration =
+      RequestConfiguration(testDeviceIds: ['30E815CD1E8D3F02F8402FDE4797BE29']);
   MobileAds.instance.updateRequestConfiguration(configuration);
   setUp();
   runApp(const MyApp());
@@ -27,8 +29,14 @@ class MyApp extends StatelessWidget {
             background: const Color.fromRGBO(33, 37, 48, 1)),
         useMaterial3: true,
       ),
-      initialRoute: "/home",
-      routes: {"/home": (context) => BlocProvider(create: (context) => getIt<HomeBloc>(), child: HomePage(),)},
+      initialRoute: "/splash_screen",
+      routes: {
+        "/home": (context) => BlocProvider(
+              create: (context) => getIt<HomeBloc>(),
+              child: HomePage(),
+            ),
+        "/splash_screen": (context) => const SplashScreen()
+      },
     );
   }
 }
---
+++ lib/ui/page/splash_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, "/home");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Image.asset('assets/images/ebook_icon.png', width: 100, height: 100,),),
+    );
+  }
+}
