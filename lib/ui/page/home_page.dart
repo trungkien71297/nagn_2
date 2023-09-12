a4567c8fb23827c1fab79f246a8b4ca78343cd9a
trungkien71297 <trungkien71297@gmail.com>
2023-09-12 08:47:39 +0700

    1.0.2+5 - Fix UI - Crash Pixel 2 virtual Play store

--- lib/ui/page/home_page.dart
+++ lib/ui/page/home_page.dart
@@ -20,197 +20,199 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     context.read<HomeBloc>().add(HomeInit());
     return Scaffold(
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(
-                  height: 10,
-                ),
-                BlocListener<HomeBloc, HomeState>(
-                  listenWhen: (prev, current) => current is HomeGetFilesStatus,
-                  listener: (context, state) {
-                    if (state is HomeGetFilesStatus) {
-                      if (state.status == ProcessStatus.failed) {
-                        showDialog(
-                            context: context,
-                            builder: (ctx) {
-                              return AlertDialog(
-                                title: const Text(
-                                  "ERROR!",
-                                  style: TextStyle(color: Colors.red),
-                                ),
-                                content: Text(
-                                  "Can't load ebook, please try again!\n${state.message}",
-                                  maxLines: 2,
-                                ),
-                                backgroundColor: Colors.black,
-                                icon: const Icon(Icons.error_outline_outlined),
-                                actions: [
-                                  TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: const Text("OK"))
-                                ],
-                              );
-                            });
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0, bottom: 15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  BlocListener<HomeBloc, HomeState>(
+                    listenWhen: (prev, current) => current is HomeGetFilesStatus,
+                    listener: (context, state) {
+                      if (state is HomeGetFilesStatus) {
+                        if (state.status == ProcessStatus.failed) {
+                          showDialog(
+                              context: context,
+                              builder: (ctx) {
+                                return AlertDialog(
+                                  title: const Text(
+                                    "ERROR!",
+                                    style: TextStyle(color: Colors.red),
+                                  ),
+                                  content: Text(
+                                    "Can't load ebook, please try again!\n${state.message}",
+                                    maxLines: 2,
+                                  ),
+                                  backgroundColor: Colors.black,
+                                  icon: const Icon(Icons.error_outline_outlined),
+                                  actions: [
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text("OK"))
+                                  ],
+                                );
+                              });
+                        }
                       }
-                    }
-                  },
-                  child: ElevatedButton.icon(
-                      onPressed: () async {
-                        final res = await _checkPermission(context);
-                        if (res) {
-                          FilePickerResult? result = await FilePicker.platform
-                              .pickFiles(
-                                  type: FileType.custom,
-                                  allowedExtensions: ['epub']);
-                          if (result != null) {
-                            File file = File(result.files.single.path!);
-                            if (context.mounted) {
-                              context.read<HomeBloc>().add(OnAddFile(file));
-                            }
-                          } else {}
-                        }
-                      },
-                      icon: const Icon(Icons.add_circle_outline),
-                      label: const Text("Add file")),
-                ),
-                Expanded(
-                    child: Stack(
-                  children: [
-                    Column(
-                      children: [
-                        const SizedBox(
-                          height: 10,
-                        ),
-                        Expanded(
-                          child: PageView(
-                            controller: _pageController,
-                            scrollDirection: Axis.vertical,
-                            physics: const NeverScrollableScrollPhysics(),
-                            children: [_coverPage(context), _infoPage(context)],
-                          ),
-                        )
-                      ],
-                    ),
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: SegmentedWidget(
-                          onChangeSegment: (index) =>
-                              _pageController.animateToPage(index,
-                                  duration: const Duration(milliseconds: 300),
-                                  curve: Curves.bounceIn),
-                        ),
-                      ),
-                    )
-                  ],
-                )),
-                const AdBanner(),
-                const SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    StreamBuilder(
-                      stream: context.read<HomeBloc>().isSaved,
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          final state = snapshot.data!;
-                          Widget? dialog;
-                          switch (state.status) {
-                            case ProcessStatus.success:
-                              dialog = AlertDialog(
-                                title: const Text("SUCCESS"),
-                                content: const Text("Save file successfully"),
-                                backgroundColor: Colors.black,
-                                icon: const Icon(Icons.done_outline_rounded),
-                                actions: [
-                                  //TODO: Do later
-
-                                  // TextButton(
-                                  //     onPressed: () {},
-                                  //     child: const Text("Go to folder")),
-                                  TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: const Text("OK"))
-                                ],
-                              );
-                              break;
-                            case ProcessStatus.failed:
-                              dialog = errorDialog(context,
-                                  "Can't save file. try again!\n${state.message}");
-                              break;
-                            default:
-                              break;
-                          }
-                          if (dialog != null) {
-                            WidgetsBinding.instance
-                                .addPostFrameCallback((timeStamp) {
-                              showDialog(
-                                context: context,
-                                builder: (context) => dialog!,
-                              );
-                            });
-                          }
-                        }
-                        return ElevatedButton.icon(
-                            onPressed: () async {
-                              final res = await _checkPermission(context);
-                              if (res) {
-                                if (context.mounted) {
-                                  context.read<HomeBloc>().add(OnSaveFile());
-                                }
+                    },
+                    child: ElevatedButton.icon(
+                        onPressed: () async {
+                          final res = await _checkPermission(context);
+                          if (res) {
+                            FilePickerResult? result = await FilePicker.platform
+                                .pickFiles(
+                                    type: FileType.custom,
+                                    allowedExtensions: ['epub']);
+                            if (result != null) {
+                              File file = File(result.files.single.path!);
+                              if (context.mounted) {
+                                context.read<HomeBloc>().add(OnAddFile(file));
                               }
-                            },
-                            icon: const Icon(Icons.save_as),
-                            label: const Text("Save"));
-                      },
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 15,
-                )
-              ],
+                            } else {}
+                          }
+                        },
+                        icon: const Icon(Icons.add_circle_outline),
+                        label: const Text("Add file")),
+                  ),
+                  Expanded(
+                      child: Stack(
+                    children: [
+                      Column(
+                        children: [
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Expanded(
+                            child: PageView(
+                              controller: _pageController,
+                              scrollDirection: Axis.vertical,
+                              physics: const NeverScrollableScrollPhysics(),
+                              children: [_coverPage(context), _infoPage(context)],
+                            ),
+                          )
+                        ],
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: SegmentedWidget(
+                            onChangeSegment: (index) =>
+                                _pageController.animateToPage(index,
+                                    duration: const Duration(milliseconds: 300),
+                                    curve: Curves.bounceIn),
+                          ),
+                        ),
+                      )
+                    ],
+                  )),
+                  const AdBanner(),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      StreamBuilder(
+                        stream: context.read<HomeBloc>().isSaved,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            final state = snapshot.data!;
+                            Widget? dialog;
+                            switch (state.status) {
+                              case ProcessStatus.success:
+                                dialog = AlertDialog(
+                                  title: const Text("SUCCESS"),
+                                  content: const Text("Save file successfully"),
+                                  backgroundColor: Colors.black,
+                                  icon: const Icon(Icons.done_outline_rounded),
+                                  actions: [
+                                    //TODO: Do later
+
+                                    // TextButton(
+                                    //     onPressed: () {},
+                                    //     child: const Text("Go to folder")),
+                                    TextButton(
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                        child: const Text("OK"))
+                                  ],
+                                );
+                                break;
+                              case ProcessStatus.failed:
+                                dialog = errorDialog(context,
+                                    "Can't save file. try again!\n${state.message}");
+                                break;
+                              default:
+                                break;
+                            }
+                            if (dialog != null) {
+                              WidgetsBinding.instance
+                                  .addPostFrameCallback((timeStamp) {
+                                showDialog(
+                                  context: context,
+                                  builder: (context) => dialog!,
+                                );
+                              });
+                            }
+                          }
+                          return ElevatedButton.icon(
+                              onPressed: () async {
+                                final res = await _checkPermission(context);
+                                if (res) {
+                                  if (context.mounted) {
+                                    context.read<HomeBloc>().add(OnSaveFile());
+                                  }
+                                }
+                              },
+                              icon: const Icon(Icons.save_as),
+                              label: const Text("Save"));
+                        },
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  )
+                ],
+              ),
             ),
-          ),
-          StreamBuilder(
-            stream: context.read<HomeBloc>().isLoading,
-            initialData: HomeLoadStatus(false, false),
-            builder: (context, snapshot) {
-              final state = snapshot.data!;
-              return state.isLoading
-                  ? state.isSave
-                      ? Container(
-                          color: Colors.blueGrey.withOpacity(0.3),
-                          child: Center(child: loading),
-                        )
-                      : Container(
-                          color: Colors.blueGrey.withOpacity(0.3),
-                          child: const Center(
-                            child: CircularProgressIndicator(),
-                          ))
-                  : Container();
-            },
-          )
-        ],
+            StreamBuilder(
+              stream: context.read<HomeBloc>().isLoading,
+              initialData: HomeLoadStatus(false, false),
+              builder: (context, snapshot) {
+                final state = snapshot.data!;
+                return state.isLoading
+                    ? state.isSave
+                        ? Container(
+                            color: Colors.blueGrey.withOpacity(0.3),
+                            child: Center(child: loading),
+                          )
+                        : Container(
+                            color: Colors.blueGrey.withOpacity(0.3),
+                            child: const Center(
+                              child: CircularProgressIndicator(),
+                            ))
+                    : Container();
+              },
+            )
+          ],
+        ),
       ),
     );
   }
